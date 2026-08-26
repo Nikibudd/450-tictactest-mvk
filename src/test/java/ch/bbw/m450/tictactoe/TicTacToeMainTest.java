21b155ddad861150481a3fcb7687ba42136e7d89
Nik Brönnimann <nik.broennimann@wilmaa.com>
2026-08-26 12:58:32 +0200

    fixed test and made simple pipeline

--- src/test/java/ch/bbw/m450/tictactoe/TicTacToeMainTest.java
+++ src/test/java/ch/bbw/m450/tictactoe/TicTacToeMainTest.java
@@ -3,9 +3,11 @@ package ch.bbw.m450.tictactoe;
 import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TicTacToeMainTest implements WithAssertions {
     @Test
     void dummyTest() {
-        assertThat(true);
+        assertTrue(true);
     }
 }
